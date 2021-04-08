758acbcfa4745b408d69a6c450870f8a2508d3a4
paul.diehl <paul.diehl@availcarsharing.com>
2021-04-07 15:53:19 -1000

    revert back to problem

--- be/index.test.js
+++ be/index.test.js
@@ -3,19 +3,6 @@ const supertest = require("supertest");
 const { readData } = require('./helpers')
 const app = createServer();
 
-test("POST /posts", async () => {
-    const data = { post: "something" }
-    await supertest(app)
-        .post("/api/posts")
-        .send(data)
-        .expect(200)
-        .then(async (response) => {
-            // Check the response
-            expect(response.body.post).toBe(data.post);
+test("empty", async () => {
 
-            // Check the data in the database
-            const posts = await readData();
-            expect(posts[posts.length - 1]).toMatchObject(data)
-
-        });
 });
--- fe/src/App.test.js
+++ fe/src/App.test.js
@@ -2,17 +2,7 @@ import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
 import App from './App';
 
-test('enter text and click save should make POST call to /api/posts with correct body', async () => {
+test('empty test', async () => {
   render(<App />);
-  const sampleText = "some test entry";
-  const samplePayload = { post: sampleText };
-  const text = screen.getByRole('textbox');
-  const button = screen.getByRole('button');
-  userEvent.type(text, sampleText);
-  userEvent.click(button);
-  await waitFor( () => expect(fetch.mock.calls[0][1].method).toEqual('POST'));
-  expect(fetch.mock.calls[0][0]).toEqual('/api/posts');
-  expect(fetch.mock.calls[0][1].body).toEqual(JSON.stringify(samplePayload));
 });
-
 
